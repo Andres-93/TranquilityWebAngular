a027332dc9c2c57b2306d2c373e665705eaa6e46
Andres-93 <andres.pgomez93@gmail.com>
2020-09-15 18:33:33 +0200

    cambios

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+// Serve static files....
+app.use(express.static(__dirname + '/dist/TranquilityWeb'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/TranquilityWeb/index.html'));
 });
 
 // default Heroku PORT
